abc4e5e85710edd13c43b3aa41478a050780c237
Aditya Sharma <aadisharma.in@gmail.com>
2023-09-10 13:06:56 +0000

    cccc

--- components/FileUploader.js
+++ components/FileUploader.js
@@ -1,11 +1,22 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-}
+// Welcome to AssemblyAI! Get started with the API by transcribing
+// a file using JavaScript.
+//
+// In this example, we'll transcribe a local file. Get started by
+// downloading the snippet, then update the 'filename' variable
+// to point to the local path of the file you want to upload and
+// use the API to transcribe.
+//
+// IMPORTANT: Update line 100 to point to a local file.
+//
+// Have fun!
+    
+const fs = require("fs");
 
-export async function upload_file(api_token, path) {
+// Your API token is already set in this variable
+const API_TOKEN = "7cf8590828bb4dff97077f1baf23df6d";
+
+// Function to upload a local file to the AssemblyAI API
+async function upload_file(api_token, path) {
   console.log(`Uploading file: ${path}`);
 
   // Read the file data
@@ -37,7 +48,8 @@ export async function upload_file(api_token, path) {
   }
 }
 
-export async function transcribeAudio(api_token, audio_url) {
+// Async function that sends a request to the AssemblyAI transcription API and retrieves the transcript
+async function transcribeAudio(api_token, audio_url) {
   console.log("Transcribing audio... This might take a moment.");
 
   // Set the headers for the request, including the API token and content type
@@ -80,3 +92,31 @@ export async function transcribeAudio(api_token, audio_url) {
     }
   }
 }
+
+// Upload a file and create a transcript using the AssemblyAI API
+async function main() {
+  console.log("Welcome to AssemblyAI!");
+
+  // -----------------------------------------------------------------------------
+  // Update the file path here, pointing to a local audio or video file.
+  // If you don't have one, download a sample file: https://storage.googleapis.com/aai-web-samples/espn-bears.m4a
+  // You may also remove the upload step and update the 'audio_url' parameter in the
+  // 'transcribeAudio' function to point to a remote audio or video file.
+  // -----------------------------------------------------------------------------
+  const path = "./espn-bears.m4a";
+  const uploadUrl = await upload_file(API_TOKEN, path);
+
+  // If the upload fails, log an error and return
+  if (!uploadUrl) {
+    console.error(new Error("Upload failed. Please try again."));
+    return;
+  }
+
+  // Transcribe the audio file using the upload URL
+  const transcript = await transcribeAudio(API_TOKEN, uploadUrl);
+
+  // Print the completed transcript object
+  console.log("Transcript:", transcript.text);
+}
+
+main();
